76324d70980e244cc892b04e7fc4586ede504a2c
hichana <hichana@gmail.com>
2023-02-22 09:09:15 +0900

    ran prettier and lint with .md files ignored

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1 class="text-3xl font-bold underline bg-red-400 ">
-  Hello world!
-</h1>
+<h1 class="text-3xl font-bold underline bg-red-400 ">Hello world!</h1>
