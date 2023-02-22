dcf5bdafa071867a03becc245fdcec023a737ab4
hichana <hichana@gmail.com>
2023-02-22 09:33:05 +0900

    routes/+layout: add placeholder login nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.css';
 </script>
 
-<slot />
+<nav class="bg-gray-800">
+    <button
+        type="button"
+        class=" bg-gray-800 p-1 text-orange-500 hover:text-white focus:outline-none"
+    >
+        Sign up / Login
+    </button>
+</nav>
+
+<main>
+    <div class="container mx-auto md:pt-28">
+        <div class="border-8  border-dashed border-pink-500 ">
+            <slot />
+        </div>
+    </div>
+</main>
