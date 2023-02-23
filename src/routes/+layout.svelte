b61e193c277d29dc05290c73adc1290ba3ed9aaf
hichana <hichana@gmail.com>
2023-02-23 15:54:27 +0900

    routes/+layout: add funcitonality back to login/oout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,35 @@
 	import { unauthenticate, logIn } from '#lib/actions';
 </script>
 
-<nav
-	class="bg-slate-300 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0"
->
+<nav class="bg-slate-300 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0">
 	<div class="container flex items-center justify-between mx-auto">
 		<a href="https://strands.so/" class="flex items-center">
 			<span class="font-semibold text-xl tracking-tight">STRANDS 三</span>
 		</a>
 		<div class="flex">
-			<button
-				type="button"
-				class="text-black bg-slate-700 bg-opacity-30 border-slate-900 border-2 hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-full text-sm px-5 py-1.5 text-center mr-3 md:mr-0"
-				>Sign Up / Login</button
-			>
+			{#if $user}
+				{#if $user?.loggedIn}
+					<button
+						type="button"
+						on:click={unauthenticate}
+						class="text-black bg-slate-700 bg-opacity-30 border-slate-900 border-2 hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-full text-sm px-5 py-1.5 text-center mr-3 md:mr-0"
+					>
+						<span class="sr-only">Log out</span>
+						Log out
+					</button>
+				{:else}
+					<button
+						type="button"
+						on:click={logIn}
+						class="text-black bg-slate-700 bg-opacity-30 border-slate-900 border-2 hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-full text-sm px-5 py-1.5 text-center mr-3 md:mr-0"
+					>
+						<span class="sr-only">Log In/Sign Up</span>
+						Log In/Sign Up</button
+					>
+				{/if}
+			{:else}
+				<p>Loading login status##########################3</p>
+			{/if}
 		</div>
 	</div>
 </nav>
