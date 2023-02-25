48d1055aff03993e509f173ad9fadd1200305487
hichana <hichana@gmail.com>
2023-02-25 18:29:50 +0900

    components/CTA: style/layout

--- src/components/CTA.svelte
+++ src/components/CTA.svelte
@@ -1,8 +1,8 @@
-<div class="flex flex-col pt-28">
-	<p class="font-sans text-6xl text-center">
+<div class="flex flex-col pt-28 items-center">
+	<p class="text-5xl md:text-6xl text-center">
         Digital DNA on Flow
     </p>
-	<p class="text-xl leading-8 text-center my-6">
+	<p class="text-md md:text-xl leading-6 md:leading-10 text-center my-6 text-pink-300 max-w-2xl px-4 md:p-0">
 		NFTs are not just for collecting, they're building blocks to create new experiences on top of
 		existing ones. With STRAND, the NFT assets in your wallet are used to craft one-of-a-kind, never
 		duplicated, digital DNA to use across future experiences on Flow.
