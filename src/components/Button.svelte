5c7a654663c44e516abcd50a52f53bfe67f72900
hichana <hichana@gmail.com>
2023-02-27 20:46:56 +0900

    prettier

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -7,7 +7,7 @@
 <button
 	type="button"
 	on:click={onClick}
-	class=" mr-3 rounded-full border-2 border-phosgreen bg-slate-600 bg-opacity-30 px-5 py-1.5 text-center text-sm font-extralight text-phosgreen hover:bg-opacity-50 focus:outline-none focus:ring-4 focus:ring-pink-500 md:mr-0"
+	class="mr-3 rounded-full border-2 border-phosgreen bg-slate-600 bg-opacity-30 px-5 py-1.5 text-center text-sm font-extralight text-phosgreen hover:bg-opacity-50 focus:outline-none focus:ring-4 focus:ring-pink-500 md:mr-0"
 >
 	{#if buttonLink}
 		<span class="sr-only">{buttonText}</span>
