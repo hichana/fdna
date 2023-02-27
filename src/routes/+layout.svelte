f90a9ef3c7c51a72a01eed770a48c68665566ab9
hichana <hichana@gmail.com>
2023-02-27 15:39:22 +0900

    test commit vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 					<Button buttonText="Log In/Sign Up" onClick={logIn} />
 				{/if}
 			{:else}
-				<p>Loading login status##########################3</p>
+				<p>Loading login status##########################</p>
 			{/if}
 		</div>
 	</div>
