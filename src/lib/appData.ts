e8bbfba9bb582311c8d19ffe8b2d9b73f8baf5d7
hichana <hichana@gmail.com>
2023-03-01 12:49:14 +0900

    lib/appData: update faq

--- src/lib/appData.ts
+++ src/lib/appData.ts
@@ -4,10 +4,6 @@ export const faq = [
 		a: 'Get two or more Flow NFTs, then build your digital DNA here and mint!'
 	},
 	{
-		q: "Where's the roadmap?",
-		a: "Not every project actually delivers on their roadmap, so beyond making cool little digital DNA NFTs on Flow, we're not promising things that might not happen. Want a roadmap? Come into our Discord, chat, share your STRAND of DNA, and help us find out what to do next! PS. for a counter-example to NFT projects that over-promise yet under-deliver, see Flovatar's site and scroll down to their roadmap. They're geniuses at continually building and delivering value for their community."
-	},
-	{
 		q: 'How does STRAND work?',
 		a: "On Flow, we have something called the NFT Metadata Standard. Almost all NFT projects on Flow now expose project, collection and NFT data through this standard, open for anyone to consume. In blockchain, this is one example of 'composability', as this project and others can use that on-chain data for other experiences. When you mint a STRANDS of Flow DNA, you use the metadata from NFTs in your wallet to build a digital double helix of Flow NFT identifiers that become base pairs. We don't actually touch your NFTs, we just make *references* to them to create your DNA. Each side of the double helix strand itself has two strands, strandA and strandB. For each of those strands, the unique permutation of NFT types (the composition and order) are memorialized on the Flow blockchain, so they can never be minted twice."
 	},
