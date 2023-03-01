625602f766baecd966f8b2a33e1ef0d85c343272
hichana <hichana@gmail.com>
2023-03-01 11:14:53 +0900

    lib/appData: update faq

--- src/lib/appData.ts
+++ src/lib/appData.ts
@@ -4,16 +4,8 @@ export const faq = [
 		a: 'Get two or more Flow NFTs, then build your digital DNA here and mint!'
 	},
 	{
-		q: 'How can I flip a STRANDS NFT to score a sweet profit?',
-		a: 'Shut up.'
-	},
-	{
 		q: "Where's the roadmap?",
 		a: "Not every project actually delivers on their roadmap, so beyond making cool little digital DNA NFTs on Flow, we're not promising things that might not happen. Want a roadmap? Come into our Discord, chat, share your STRAND of DNA, and help us find out what to do next! PS. for a counter-example to NFT projects that over-promise yet under-deliver, see Flovatar's site and scroll down to their roadmap. They're geniuses at continually building and delivering value for their community."
-	},
-	{
-		q: "Where's the white paper, the litepaper!?",
-		a: 'Shut up.'
 	},
 	{
 		q: 'How does STRAND work?',
